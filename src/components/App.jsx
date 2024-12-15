048c4de9dcb843e97c730eb1e978f3e5529a43f7
Mykhailo Kalinichenko <99479355+fidelmigel@users.noreply.github.com>
2024-12-15 13:38:16 +0100

    modal, cleanup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,29 @@
+import { useState } from "react";
 import { ColorPicker } from "../components/ColorPicker/ColorPicker";
 import { Counter } from "../components/Counter/Counter";
 import { TodoList } from "../components/TodoList/TodoList";
+import Modal from "../components/Modal/Modal";
 
 const App = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
   return (
     <div>
-      <h1>State</h1>
       <Counter />
       <ColorPicker />
       <TodoList />
+      <button onClick={openModal}>Open modal</button>
+      {isOpen && (
+        <Modal onClose={closeModal}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam saepe
+          voluptatum blanditiis assumenda maxime quam ipsum, ratione sed
+          similique quo ipsa quia minima quisquam inventore numquam culpa omnis
+          itaque corrupti?
+        </Modal>
+      )}
     </div>
   );
 };
+
 export default App;
