a6f6d07e214b091a9f8b4b23fcc7be336711cd56
Mykhailo Kalinichenko <99479355+fidelmigel@users.noreply.github.com>
2024-12-20 22:46:46 +0100

    formic basic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import { Counter } from "../components/Counter/Counter";
 import { TodoList } from "../components/TodoList/TodoList";
 import Modal from "../components/Modal/Modal";
 import Vote from "../components/Vote/Vote";
-import Form from "../components/Form/Form";
+import FormRegister from "../components/Form/Form";
 import ControlledForm from "../components/Form/ControlledForm";
 import FullForm from "../components/Form/FullForm";
 
@@ -28,8 +28,8 @@ const App = () => {
         </Modal>
       )}
 
-      <h1>Form</h1>
-      <Form />
+      <h1>🤖FORMIK🤖</h1>
+      <FormRegister />
       <h1>ControlledForm </h1>
       <ControlledForm />
       <h1>FullForm </h1>
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,38 +1,43 @@
+import React from "react";
 import s from "./Form.module.css";
+import { Field, Form, Formik } from "formik";
 
-const Form = () => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    const username = e.target.elements.username.value;
-    const email = e.target.elements.email.value;
-    const password = e.target.elements.password.value;
-
-    console.log({ username, email, password });
-    e.target.reset();
+const FormRegister = () => {
+  const handleSubmit = (values, actions) => {
+    console.log(values);
+    actions.resetForm();
   };
 
   return (
     <div className={s.formWrapper}>
-      <form onSubmit={handleSubmit} className={s.form}>
-        <label className={s.label}>
-          <span>Name:</span>
-          <input className={s.input} type="text" name="username" />
-        </label>
-        <label className={s.label}>
-          <span>Email:</span>
-          <input className={s.input} type="text" name="email" />
-        </label>
-        <label className={s.label}>
-          <span>Password:</span>
-          <input className={s.input} type="password" name="password" />
-        </label>
-        <button className={s.button} type="submit">
-          Submit
-        </button>
-      </form>
+      <Formik
+        onSubmit={handleSubmit}
+        initialValues={{
+          username: "",
+          email: "",
+          password: "",
+        }}
+      >
+        <Form className={s.form}>
+          <label className={s.label}>
+            <span>Name:</span>
+            <Field className={s.input} type="text" name="username" />
+          </label>
+          <label className={s.label}>
+            <span>Email:</span>
+            <Field className={s.input} type="text" name="email" />
+          </label>
+          <label className={s.label}>
+            <span>Password:</span>
+            <Field className={s.input} type="password" name="password" />
+          </label>
+          <button className={s.button} type="submit">
+            Submit
+          </button>
+        </Form>
+      </Formik>
     </div>
   );
 };
 
-export default Form;
+export default FormRegister;
